84ca396278268740a31bb662c80c4f5ac37c73fb
lesion <lesion@autistici.org>
2018-03-28 16:29:23 +0200

    clean server.js

--- example/server.js
+++ example/server.js
@@ -1,27 +1,26 @@
-const Armadietto = require('../lib/armadietto');
-let store,
-  server;
+const path = require('path')
+const Armadietto = require('../lib/armadietto')
+let store
+let server
 
-const  type = process.argv[2];
+const type = process.argv[2]
 
-if (type === 'redis')
-  store = new Armadietto.Redis({database: 3});
-else
-  store = new Armadietto.FileTree({path: __dirname + '/tree'});
+if (type === 'redis') store = new Armadietto.Redis({database: 3})
+else store = new Armadietto.FileTree({path: path.join(__dirname, 'tree')})
 
 server = new Armadietto({
-  store:  store,
-  http:   {port: 8080},
-  https:  {
-    force:  true,
-    port:   443,
-    cert:   __dirname + '/ssl/server.crt',
-    key:    __dirname + '/ssl/server.key'
+  store,
+  http: {port: 8080},
+  https: {
+    force: true,
+    port: 443,
+    cert: path.join(__dirname, '/ssl/server.crt'),
+    key: path.join(__dirname, '/ssl/server.key')
   },
   allow: {
     signup: true
   },
   cacheViews: false
-});
+})
 
-server.boot();
+server.boot()

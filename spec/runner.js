71a6afa68c9c3acb7ed038e6d75585eb930a1823
lesion <lesion@autistici.org>
2018-03-30 23:48:15 +0200

    start refactoring unit test with mocha/chai

--- spec/runner.js
+++ spec/runner.js
@@ -1,13 +1,11 @@
-process.env.SILENT = '1'
+// process.env.SILENT = '1'
 
-var JS = require('jstest')
-
-require('./armadietto/web_finger_spec')
-require('./armadietto/oauth_spec')
+//require('./armadietto/web_finger_spec')
+// require('./armadietto/oauth_spec')
 require('./armadietto/storage_spec')
 
-require('./store_spec.js')
-require('./stores/file_tree_spec')
-require('./stores/redis_spec')
+// require('./store_spec.js')
+// require('./stores/file_tree_spec')
+// require('./stores/redis_spec')
 
-JS.Test.autorun()
+// JS.Test.autorun()

226739c0eefafbd4e74c8a8771fc0fd6ada8459e
Jakub Ner <jakub.at.work@gmail.com>
2022-03-19 15:48:18 -0700

    Minor fixes.

--- example/server.js
+++ example/server.js
@@ -7,7 +7,7 @@ const type = process.argv[2];
 if (type === 'redis') store = new Armadietto.Redis({ database: 3 });
 else store = new Armadietto.FileTree({ path: path.join(__dirname, 'tree') });
 const middleware = [ // order matters:  on each request middleware called in order defined below
-  require('../lib/extensions/storage_allowance/storage_allowance'),
+  require('../lib/extensions/storage_allowance/storage_allowance')
 ];
 const server = new Armadietto({
   store,
@@ -19,11 +19,11 @@ const server = new Armadietto({
     signup: true
   },
   cacheViews: false,
-  "extensions": {
-    "storage_allowance": {
-      "enabled": true,
-      "max_bytes": 10485760,
-      "salt": "c0c0nut"
+  extensions: {
+    storage_allowance: {
+      enabled: true,
+      max_bytes: 10485760,
+      salt: 'c0c0nut'
     }
   }
 });

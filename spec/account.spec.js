94db9bd35bda2cf4ec306e0dab2042d3791dfde3
P. Douglas Reeder <reeder.29@gmail.com>
2024-03-19 16:52:18 -0400

    Modular: each streaming store decides what usernames are valid

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -8,37 +8,37 @@ chai.use(require('chai-as-promised'));
 module.exports.shouldCreateDeleteAndReadAccounts = function () {
   describe('createUser', function () {
     before(function () {
-      this.username1 = 'automated-test-' + Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+      this.username = 'automated-test-' + Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
     });
 
     after(async function () {
-      await this.store.deleteUser(this.username1);
+      await this.store.deleteUser(this.username);
     });
 
     it('rejects a user with too short a name', async function () {
-      const params = { username: 'a', email: 'a@b.c', password: 'swordfish' };
-      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, 'Username must be at least 2 characters');
+      const params = { username: 'aa', email: 'a@b.c', password: 'swordfish' };
+      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, /user\s?name/i);
     });
 
     it('rejects creating a user with illegal characters in username', async function () {
-      const params = { username: 'a+a', email: 'a@b.c', password: 'swordfish' };
-      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, 'only contain lowercase letters, numbers, dots, dashes and underscores');
+      const params = { username: this.username + '\\q', email: 'a@b.c', password: 'swordfish' };
+      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, /user\s?name/i);
     });
 
     it('rejects creating a user with bad email', async function () {
-      const params = { username: 'a1a', email: 'a@b', password: 'swordfish' };
-      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, 'Email is not valid');
+      const params = { username: this.username + 'j', email: 'a@b', password: 'swordfish' };
+      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, /email/i);
     });
 
     it('rejects creating a user without password', async function () {
-      const params = { username: 'a2a', email: 'a@b.c', password: '' };
-      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, 'Password must not be blank');
+      const params = { username: this.username + 'h', email: 'a@b.c', password: '' };
+      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, /password/i);
     });
 
     it('creates a user & rejects creating a new user with an existing name', async function () {
-      const params1 = { username: this.username1, email: 'a@b.c', password: 'swordfish' };
-      await expect(this.store.createUser(params1)).to.eventually.eql(this.username1);
-      const params2 = { username: this.username1, email: 'd@e.f', password: 'iloveyou' };
+      const params1 = { username: this.username, email: 'a@b.c', password: 'swordfish' };
+      await expect(this.store.createUser(params1)).to.eventually.eql(this.username + this.USER_NAME_SUFFIX);
+      const params2 = { username: this.username, email: 'd@e.f', password: 'iloveyou' };
       await expect(this.store.createUser(params2)).to.be.rejectedWith(Error, 'is already taken');
     });
   });
@@ -54,7 +54,7 @@ module.exports.shouldCreateDeleteAndReadAccounts = function () {
 
     it('deletes a user', async function () {
       const params = { username: this.username2, email: 'a@b.c', password: 'swordfish' };
-      await expect(this.store.createUser(params)).to.eventually.eql(this.username2);
+      await expect(this.store.createUser(params)).to.eventually.eql(this.username2 + this.USER_NAME_SUFFIX);
       const result = await this.store.deleteUser(this.username2);
       await expect(result?.[0]).to.be.greaterThanOrEqual(2);
       await expect(result?.[1]).to.equal(0);
--- spec/streaming_handler/S3Handler.spec.js
+++ spec/streaming_handler/S3Handler.spec.js
@@ -2,6 +2,8 @@
 /* eslint-env mocha, chai, node */
 /* eslint-disable no-unused-expressions */
 
+const chai = require('chai');
+const expect = chai.expect;
 const s3handler = require('../../lib/routes/S3Handler');
 const { shouldStoreStream } = require('../streaming_handler.spec');
 const { configureLogger } = require('../../lib/logger');
@@ -10,12 +12,20 @@ const { shouldCreateDeleteAndReadAccounts } = require('../account.spec');
 describe('S3 streaming handler', function () {
   before(function () {
     configureLogger({ stdout: [], log_dir: './test-log', log_files: ['debug'] });
+    this.USER_NAME_SUFFIX = '-java.extraordinary.org';
     // If the environment variables aren't set, tests are run using a shared public account on play.min.io
-    this.handler = s3handler(process.env.S3_ENDPOINT, process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY);
+    this.handler = s3handler(process.env.S3_ENDPOINT, process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY, undefined, this.USER_NAME_SUFFIX);
     this.store = this.handler;
   });
 
   shouldCreateDeleteAndReadAccounts();
+
+  describe('createUser (S3-specific)', function () {
+    it('rejects a user with too long a name for the user name suffix', async function () {
+      const params = { username: 'a-------10--------20--------30--------40-', email: 'a@b.c', password: 'swordfish' };
+      await expect(this.store.createUser(params)).to.be.rejectedWith(Error, '3–40 characters long');
+    });
+  });
 
   shouldStoreStream();
 });

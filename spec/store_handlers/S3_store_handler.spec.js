643f34bf5d086fb8afcbf4a31dcedd279feaad53
P. Douglas Reeder <reeder.29@gmail.com>
2025-10-11 21:37:21 -0400

    S3 store: folder data is calculated from S3 list and HEAD commands, then cached

--- spec/store_handlers/S3_store_handler.spec.js
+++ spec/store_handlers/S3_store_handler.spec.js
@@ -1,13 +1,20 @@
 // If a environment variables aren't set, tests are run using a shared public account on play.min.io
 /* eslint-env mocha, chai, node */
 /* eslint-disable no-unused-expressions */
+/* eslint no-unused-vars: ["warn", { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }]  */
 
 const chai = require('chai');
 const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+const { posix } = require('node:path');
 const s3storeHandler = require('../../lib/routes/S3_store_router');
 const { shouldStoreStreams } = require('../store_handler.spec');
 const { configureLogger } = require('../../lib/logger');
 const { shouldCreateDeleteAndReadAccounts } = require('../account.spec');
+const callMiddleware = require('../util/callMiddleware');
+const { NoSuchKey } = require('@aws-sdk/client-s3');
+
+const BLOB_PREFIX = 'remoteStorageBlob/';
 
 describe('S3 store router', function () {
   before(function () {
@@ -31,4 +38,48 @@ describe('S3 store router', function () {
   });
 
   shouldStoreStreams();
+
+  describe('folder caching', function () {
+    before(async function () {
+      this.timeout(15_000);
+
+      const usernameStore = 'automated-test-' + Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+      const user = await this.store.createUser({ username: usernameStore, contactURL: 'p@q.rr' }, new Set());
+      this.userIdStore = user.username;
+      this.bucketName = user.username + this.USER_NAME_SUFFIX;
+    });
+
+    after(async function () {
+      this.timeout(360_000);
+      await this.store.deleteUser(this.userIdStore, new Set());
+    });
+
+    it("doesn't cache folder after PUT, but does after GET", async function () {
+      const categoryPath = 's3cat/';
+      const rsPath = posix.join(categoryPath, 's3fold/s3doc');
+      const content = 'aufgenehmen';
+      const [_putReq, putRes] = await callMiddleware(this.handler, {
+        method: 'PUT',
+        url: `/${this.userIdStore}/${rsPath}`,
+        headers: { 'Content-Length': content.length, 'Content-Type': 'text/vnd.zoo.kcl' },
+        body: content
+      });
+      expect(putRes.statusCode).to.equal(201);
+      expect(putRes.get('ETag')).to.match(/^".{6,128}"$/);
+      expect(putRes._getData()).to.equal('');
+
+      await expect(this.store.readJson(this.bucketName, posix.join(BLOB_PREFIX, categoryPath)))
+        .to.be.rejectedWith(NoSuchKey);
+
+      const [_getReq, getRes] = await callMiddleware(this.handler, { method: 'GET', url: `/${this.userIdStore}/${categoryPath}` });
+      expect(getRes.statusCode).to.equal(200);
+      expect(getRes.get('Content-Type')).to.equal('application/ld+json');
+      const category = getRes._getJSONData();
+      expect(category['@context']).to.equal('http://remotestorage.io/spec/folder-description');
+      expect(Object.keys(category.items)).to.have.length(1);
+
+      const category2 = await this.store.readJson(this.bucketName, posix.join(BLOB_PREFIX, categoryPath));
+      expect(category2).to.deep.equal(category);
+    });
+  });
 });
